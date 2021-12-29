aeb51b9fb6bea6630453e96c1fe03a1c2a003b6a
NL <Lapina.n.b@mail.ru>
2021-12-30 00:20:12 +1000

    ю

--- src/Cart/Cart.jsx
+++ src/Cart/Cart.jsx
@@ -1,14 +1,14 @@
 import './Cart.css';
 import React from 'react'
 
-function Card(props) {
+function Card({url, name, info, superpower}) {
     return (
         <div className="card">
-            <img src={props.url}></img>
-            <div className='name_hero'>{props.name}</div>
-            <div className='information_hero'>{props.info}</div> 
-<div className='superpower'>{props.superpower}</div>
-</div>
+            <img src={url}></img>
+            <div className='name_hero'>{name}</div>
+            <div className='information_hero'>{info}</div> 
+            <div className='superpower'>{superpower}</div>
+        </div>
     );
 }
 
